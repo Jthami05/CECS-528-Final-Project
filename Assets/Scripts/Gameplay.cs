edb36158520d2eb03748dbac4361fc453709f779
Jthami05 <jthami05@gmail.com>
2017-03-06 21:02:26 -0500

    final version

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gameplay : MonoBehaviour {
+
+    public Transform Ball;
+	// Use this for initialization
+	void Start ()
+    {
+        //Instantiate(Ball, new Vector3(4.0f, 1.4f, 1.0f), Quaternion.identity);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TheBall.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheBall : MonoBehaviour
+{
+
+    public float speed;
+    public float contactSpeed;
+    public float speedCap;
+
+    private Rigidbody ballRB;
+
+    void Start()
+    {
+        ballRB = GetComponent<Rigidbody>();
+        Vector3 startMovement = new Vector3(0.0f, 0.0f, 120.0f);
+        ballRB.AddForce(startMovement * speed);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, -2.0f);
+
+        ballRB.AddForce(movement * speed);
+    }
+
+    //void OnCollisionEnter(Collision info)
+    //{
+    //    if(info.gameObject.tag == "Bumper")
+    //    {
+    //        var localVelocity = transform.TransformDirection(ballRB.velocity);
+    //        Vector3 contact = new Vector3(-localVelocity.x,0,-localVelocity.z);
+    //        print("Ball hit bumper!");
+    //        ballRB.AddForce(contact * contactSpeed);
+
+    //    }
+    //}
+}
